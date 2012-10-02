a04fe39093955d01f42cfe1295b87a6d7d3c4705
NIIBE Yutaka <gniibe@fsij.org>
2012-10-02 13:44:36 +0900

    add adc_init

--- src/adc.h
+++ src/adc.h
@@ -4,6 +4,7 @@ extern Thread *rng_thread;
 #define NEUG_SAMPLE_BUFSIZE     512
 extern uint16_t adc_samp[NEUG_SAMPLE_BUFSIZE];
 
+void adc_init (void);
 void adc_start (void);
 void adc_start_conversion (int offset, int size);
 void adc_stop (void);
